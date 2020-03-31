2f798ac41be3d74543d5e81b88f3e3675c5aa4dd
Lucille Chiang <lchiang.tw@ffn.com>
2020-04-01 06:03:56 +0800

    add video data

--- src/components/VideoIndex.vue
+++ src/components/VideoIndex.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="container">
-    <h1>{{ msg }}</h1>
-    <div class="video_box">
-
+    <div class="video_container">
+      <div class="video_box" v-for="(item, index) in videoItem" :key="index">
+        <div class="video_box_img" v-bind:style="{ backgroundImage: 'url(' + item.snippet.thumbnails.standard.url + ')' }">
+          <div class="video_duration" v-text="transferTimeFormat(item.contentDetails.duration)"></div>
+        </div>
+        <div class="video_title" v-text="item.snippet.title"></div>
+        <div class="video_description" v-text="item.snippet.description"></div>
+      </div>
+      
+    </div>
+    <div class="btn" v-on:click="clickTest('')">
+      click to add store
     </div>
   </div>
 </template>
@@ -26,7 +35,7 @@ export default {
   created() {
     Vue.axios.get('https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&chart=mostPopular&maxResults=12&key=AIzaSyBOdz5KQWjpP44XNJirEpIgYlKkkGisE98').then((response) => {
       let video_array_1 = response.data.items; 
-      Vue.axios.get('https://www.googleapis.com/youtube/v3/videos?pageToken=CAwQAA&part=snippet,contentDetails&chart=mostPopular&maxResults=12&key=AIzaSyBOdz5KQWjpP44XNJirEpIgYlKkkGisE98').then((response) => {
+      Vue.axios.get('https://www.googleapis.com/youtube/v3/videos?pageToken=CAwQAA&part=snippet,contentDetails&chart=mostPopular&maxResults=13&key=AIzaSyBOdz5KQWjpP44XNJirEpIgYlKkkGisE98').then((response) => {
         let video_array_2 = response.data.items;
         let video_array_all = video_array_1.concat(video_array_2);
         this.videoItem = video_array_all;
@@ -37,11 +46,25 @@ export default {
     // console.log(this.$store.state.favorite)
     // console.log(this.catchFavorite)
   },
+  methods: {
+    clickTest: function(e) {
+      this.$store.commit('pushFavorite');
+      console.log(this.$store.state.favorite)
+      console.log(e)
+    },
+    transferTimeFormat: function(str) {
+      return str.replace(/PT|S/g, '').replace(/[A-Z]/g, ':');
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
 h3 {
   margin: 40px 0 0;
 }
@@ -56,4 +79,68 @@ li {
 a {
   color: #42b983;
 }
+.video_container {
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  .video_box {
+    width: 25%;
+    padding-top: 2.5%;
+    word-wrap: break-word;
+    position: relative;
+    text-align: left;
+    .video_box_img {
+      width: 90%;
+      padding-bottom: 67.5%;
+      margin: 0 auto;
+      background-size: cover;
+      position: relative;
+      .video_duration {
+        width: fit-content;
+        padding: 0.25em 0.5em;
+        background-color: rgba(0, 0, 0, .6);
+        color: #cccccc;
+        font-size: 12px;
+        position: absolute;
+        right: 0;
+        bottom: 0;
+      }
+    }
+    .video_title {
+      color: #000000;
+      font-weight: 700;
+      padding: 0.5em 5% 0;
+      line-height: 1.2em;
+    }
+    .video_description {
+      font-size: 12px;
+      padding: 0 5%;
+      color: #cccccc;
+      max-height: 80px;
+      overflow: scroll;
+      &::-webkit-scrollbar {
+        width: 5px;
+      }
+      &:-webkit-scrollbar-track {
+        background: transparent; 
+      }
+      &::-webkit-scrollbar-thumb {
+        background: #555555; 
+        border-radius: 5px;
+      }
+      &::-webkit-scrollbar-thumb:hover {
+        background: #cccccc; 
+      }
+    }
+
+  }
+}
+.btn {
+  cursor: pointer;
+  border: solid 1px #cccccc;
+  width: fit-content;
+  padding: 5px;
+  margin: 0 auto;
+}
+
 </style>
