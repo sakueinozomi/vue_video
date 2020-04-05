664d072f6405e0fc0964cd0845e0bf024abb0462
Lucille Chiang <lchiang.tw@ffn.com>
2020-04-06 02:41:30 +0800

    add video js to play m3u8 file

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="player">
     <h1>{{ msg }}</h1>
+    <div>
+        <video id="my-video" class="video-js vjs-default-skin" controls preload="auto">
+          <source src="https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8" type="application/x-mpegURL">
+        </video>
+    </div>
   </div>
   
 </template>
 
 <script>
-// @ is an alias to /src
+import videojs from 'video.js'
+import 'videojs-contrib-hls'
+
 export default {
   name: 'Player',
   data() {
@@ -14,5 +21,16 @@ export default {
       msg: 'player_page'
     }
   },
+  mounted() {
+    videojs('my-video', {
+      bigPlayButton: true,
+      textTrackDisplay: false,
+      posterImage: true,
+      errorDisplay: false,
+      controlBar: true
+    }, function () {
+      this.play()
+    })
+  }
 }
 </script>
